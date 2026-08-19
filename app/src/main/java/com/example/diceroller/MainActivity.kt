edfabc2bbe12623164ef10fa00aab8bb28420aa2
orlane111 <nguessamiensa137@gmail.com>
2026-08-19 16:28:15 +0000

    initialisation et configuration

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerTheme {
+               DiceRollerApp()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun DiceWithButtonAndImage() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
